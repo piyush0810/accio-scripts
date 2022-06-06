1093dd78c67f4a518138959d9062553bdb1aa642
Piyush <PIYUSHKUMAR0810@GMAIL.COM>
2022-06-06 12:34:25 +0000

    Added Put Script

--- db_update_zoho.py
+++ db_update_zoho.py
@@ -27,35 +27,45 @@ cursor2 = conn2.cursor()
 #Executing an MYSQL function using the execute() method
 
 
-cursor.execute("SELECT * from zoho_temp2 limit 10000")
+cursor.execute("SELECT * from zoho_temp1 limit 10000")
 while True:
     data = cursor.fetchone()
     if(data==None):
         break
+    cursor2.execute(f"select \"leadId\" from sales_leads where \"userId\"='{data[16]}'")
     
-
+    Leadid=cursor2.fetchone()
+    if(Leadid==None):
+        continue
+    Leadid=Leadid[0]
+    
+    
     url = "https://www.zohoapis.in/crm/v2/leads"
     # print(data)
     payload = json.dumps({
     "data": [
         {
+            "id":Leadid,
         "Last_Name": str(data[0]),
         "Mobile": str(data[1]),
         "description": str(data[2]),
         "Lead_Status": "Not Qualified",
         "Lead_Source":str(data[3]),
-        "Coding_Test1_Date":data[4].strftime("%Y-%m-%dT%H:%M:%S.%fZ") if data[4] != None else str(None),
+        "Coding_Test1_Date":data[4].strftime("%Y-%m-%dT%H:%M:%S.%fZ")[:-14] if data[4] != None else None,
         "Interview_Status":str(data[5]),
         "Coding_Test_Status":str(data[6]),
-        "Coding_Test2_Date":data[7].strftime("%Y-%m-%dT%H:%M:%S.%fZ") if data[7] != None else str(None),
-        "Interview_Date":data[8].strftime("%Y-%m-%dT%H:%M:%S.%fZ") if data[8] != None else str(None),
+        "Coding_Test2_Date":data[7].strftime("%Y-%m-%dT%H:%M:%S.%fZ")[:-14] if data[7] != None else None,
+        "Interview_Date":data[8].strftime("%Y-%m-%dT%H:%M:%S.%fZ")[:-14] if data[8] != None else None,
         "Email":str(data[9]),
-        "CBR_Date_Time":str(data[10]),
+        "CBR_Date_Time":str(data[10])[:-14],
         "Call_Back_Requested1":str(data[11]),
         "Graduation_Year":str(data[12]),
         "Progress_Status":str(data[13]),
         "Questions_Solved1":str(data[14]),
-        "Last_Question_Solved":str(data[15]),
+        "Last_Question_Solved":str(data[15])[:-14] if data[15]!=None else None,
+        "IsAgreementSigned":str(data[17]),
+        "Number_of_Lectures_attended_in_precourse":data[18],
+        "Last_Lecture_Attended":str(data[19])[:-5] if data[19]!=None else None
         # "Created_Date_Time":str(data[16])
         }
     ],
@@ -66,19 +76,19 @@ while True:
     ]
     })
     headers = {
-    'Authorization': 'Zoho-oauthtoken 1000.d9d9687fe912e3a9a39383f54461d092.cba2f8248888201e8eac3fbf07a1d127',
+    'Authorization': 'Zoho-oauthtoken 1000.f59e2f48fe75759a27f296d30c8a1cbb.b482a117a7e28a65e06fda09438392cf',
     'Content-Type': 'application/json',
     }
     # print(payload)
     
-    response = requests.request("POST", url, headers=headers, data=payload)
-    
+    response = requests.request("PUT", url, headers=headers, data=payload)
+    if(json.loads(response.text)['data'][0]['message']=='the id given seems to be invalid'):
+        cursor2.execute(f"delete from sales_leads where \"userId\"='{data[16]}'")
+        conn2.commit()
     print(json.loads(response.text))
-    leadid= json.loads(response.text)['data'][0]['details']['id']
-    userid=data[16]
     
-    cursor2.execute(f"insert into sales_leads (\"userId\",\"leadId\") values ('{userid}','{leadid}')")
-    conn2.commit()
+    
+    
 
     
 
@@ -86,7 +96,7 @@ while True:
 # Fetch a single row using fetchone() method.
 # data = cursor.fetchall()
 # print("Connection established to: ",data)
-print(cursor.fetchone())
+
 #Closing the connection
 conn2.close()
 conn.close()
